8994ad9801561c0343e8d280a2e63ad8f9f9d2ea
Adhikari <vish.dba1@gmail.com>
2016-04-16 23:58:30 +0000

    Implementing Follower and Following relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :posts
   
   root 'pages#index'
